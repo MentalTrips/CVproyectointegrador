eae8d499b1210e45153a4d24923bb2cb8de62eca
MentalTrips <124063689+MentalTrips@users.noreply.github.com>
2023-02-01 10:08:10 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,16 @@
+function changeper () {
+    document.getElementById("informacion").innerHTML = "Mi nombre completo es Máximo Cozzeti, mi direccion de email es maximocozzeti@simuladores.com, fecha de nacimiento 21/11/1964, direccion 4558 Av. 9 de Julio y mi numero de telefono es (011) 11-2834-1822."
+}
+
+function changeac () {
+    document.getElementById("informacion").innerHTML = "En el 1976 termine mis estudios secundarios en la Escuela Tecnina nro 1, en 1982 comence mis estudios en Abogacia en la Universidad de Buenos Aires los cuales finalice en el año 1989."
+}
+
+function changexp () {
+    document.getElementById("informacion").innerHTML = "En la década de 1990, me enriquecí con una mesa de dinero, prestando activos y ejecutando de manera sucia las propiedades a quienes no podían pagar sus deudas. En año 1995 trabajé cómo colaborador foráneo del proyecto Blue Book de la NASA. El los 2000 comenzé en la politica, y actualmente soy Diputado de Santa Fe.."
+
+}
+
+function changeobj () {
+    document.getElementById("informacion").innerHTML = "Mi objetivo es ser un robot diseñado por los rusos para combatir al capitalismo."
+}
